a0e6353abcf9f2d334fa060644783857f6011e35
ShweZinTun <shwez959@gmail.com>
2020-07-23 01:26:12 +0630

    message

--- No2.php
+++ No2.php
@@ -6,31 +6,23 @@
     <title>Document</title>
 </head>
 <body>
-<?php
-    $name="";
-?>
+
 
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
     Please enter a string :<input type="text" name="name"><br>
-    Please enter a number :<input type="text" name="name"><br>
+    Please enter a number :<input type="text" name="num"><br>
     <input type="submit" name="submit" value="submit">   
 </form>
 
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $name = test_input($_POST["name"]);
-        $number=test_input($_POST["number"]);
-        $char=strlen($name);
+        $name = $_POST["name"];
+        $num= $_POST["num"];
+        echo substr($name,$num);
         
+          
     }
-    function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-
-    echo $name;
+    
 ?>
 </body>
 </html>
